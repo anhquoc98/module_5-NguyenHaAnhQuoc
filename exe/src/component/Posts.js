f425cd4b450b1e44e65d7e3a27b1b75d91e26977
anhquoc98 <anhquoc98@gmail.com>
2023-06-05 10:36:04 +0700

    up

--- exe/src/component/Posts.js
+++ exe/src/component/Posts.js
@@ -3,9 +3,9 @@ import {posts} from "./data";
 
 export function Posts() {
     return (
-        <div >
-            <table className='table table-danger '>
-                <thead>
+        <div>
+            <table className='table table-striped'>
+                <thead className='table-dark'>
                 <tr>
                     <th>ID</th>
                     <th>TITTLE</th>
@@ -16,25 +16,44 @@ export function Posts() {
                 </thead>
                 <tbody>
 
-                    {posts.map((item,index) =>(
-                        <tr key={index}>
-                            <td>{index+1}</td>
-                            <td>{item.title}</td>
-                            <td>{item.category}</td>
-                            <td>{item.updatedAt}</td>
-                            <td>
-                                <button type="button" className="btn btn-danger" data-toggle="modal"
-                                        data-target="#deleteModal">
-                                    delete
-                                </button>
-                                <button className='btn btn-primary m-lg-2 w-49'>edit</button>
-                            </td>
+                {posts.map((item, index) => (
+                    <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{item.title}</td>
+                        <td>{item.category}</td>
+                        <td>{item.updatedAt}</td>
+                        <td>
+                            <button type="button" className="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal">
+                                delete
+                            </button>
+                            <button className='btn btn-primary m-lg-2 w-49'>edit</button>
+                        </td>
 
-                        </tr>
-                    ))}
+                    </tr>
+                ))}
 
                 </tbody>
             </table>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
+                 aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            Bạn có muốn xóa
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-danger">Xóa</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
